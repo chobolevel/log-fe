d4a5879dd3e3b3e2b8cf0c02c19bb0950f2586bb
chobolevel <rodaka123@naver.com>
2024-08-26 23:45:41 +0900

    Refactor: add profile menu when mobile layout

--- src/components/layouts/MobileLayout/MobileLayoutHeader/MobileLayoutHeader.tsx
+++ src/components/layouts/MobileLayout/MobileLayoutHeader/MobileLayoutHeader.tsx
@@ -1,10 +1,17 @@
-import {Flex, Menu, MenuButton, MenuItem, MenuList, Text} from "@chakra-ui/react";
-import {CSSProperties} from "react";
-import {useSafePush} from "@/hooks";
-import {toUrl} from "@/utils";
-import {PageRoutes} from "@/constants";
-import {TiThMenu} from "react-icons/ti";
-import {useGetMe, useLogout} from "@/apis";
+import {
+  Flex,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Text,
+} from "@chakra-ui/react";
+import { CSSProperties } from "react";
+import { useSafePush } from "@/hooks";
+import { toUrl } from "@/utils";
+import { PageRoutes } from "@/constants";
+import { TiThMenu } from "react-icons/ti";
+import { useGetMe, useLogout } from "@/apis";
 
 const linkStyle: CSSProperties = {
   fontSize: "lg",
@@ -13,42 +20,85 @@ const linkStyle: CSSProperties = {
 };
 
 const MobileLayoutHeader = () => {
-  const { push } = useSafePush()
+  const { push } = useSafePush();
 
-  const { data: me } = useGetMe()
-  const logout = useLogout()
+  const { data: me } = useGetMe();
+  const logout = useLogout();
 
   return (
-   <Flex as={"header"} h={100} justify={"space-between"} align={"center"} px={4}>
-     <Flex>
-       <Text style={linkStyle} onClick={() => {
-         push(toUrl(PageRoutes.Home))
-       }}>
-         CHOLO
-       </Text>
-     </Flex>
-     <Flex gap={2}>
-       <Menu>
-         <MenuButton>
-           <TiThMenu size={24} />
-         </MenuButton>
-         <MenuList>
-           {me ? <MenuItem fontWeight={'bold'} onClick={() => {
-             logout()
-           }}>LOGOUT</MenuItem> : <MenuItem fontWeight={"bold"} onClick={() => {
-             push(toUrl(PageRoutes.SignIn))
-           }}>LOGIN</MenuItem>}
-           <MenuItem fontWeight={"bold"} onClick={() => {
-             push(toUrl(PageRoutes.Posts))
-           }}>ARCHIVE</MenuItem>
-           <MenuItem fontWeight={"bold"} onClick={() => {
-             push(toUrl(PageRoutes.Tags))
-           }}>TAGS</MenuItem>
-         </MenuList>
-       </Menu>
-     </Flex>
-   </Flex>
-  )
-}
+    <Flex
+      as={"header"}
+      h={100}
+      justify={"space-between"}
+      align={"center"}
+      px={4}
+    >
+      <Flex>
+        <Text
+          style={linkStyle}
+          onClick={() => {
+            push(toUrl(PageRoutes.Home));
+          }}
+        >
+          CHOLO
+        </Text>
+      </Flex>
+      <Flex gap={2}>
+        <Menu>
+          <MenuButton>
+            <TiThMenu size={24} />
+          </MenuButton>
+          <MenuList>
+            {me ? (
+              <>
+                <MenuItem
+                  fontWeight={"bold"}
+                  onClick={() => {
+                    push(toUrl(PageRoutes.Profile));
+                  }}
+                >
+                  PROFILE
+                </MenuItem>
+                <MenuItem
+                  fontWeight={"bold"}
+                  onClick={() => {
+                    logout();
+                  }}
+                >
+                  LOGOUT
+                </MenuItem>
+              </>
+            ) : (
+              <MenuItem
+                fontWeight={"bold"}
+                onClick={() => {
+                  push(toUrl(PageRoutes.SignIn));
+                }}
+              >
+                LOGIN
+              </MenuItem>
+            )}
+            <MenuItem
+              fontWeight={"bold"}
+              onClick={() => {
+                push(toUrl(PageRoutes.Posts));
+              }}
+            >
+              ARCHIVE
+            </MenuItem>
+            <MenuItem
+              fontWeight={"bold"}
+              onClick={() => {
+                push(toUrl(PageRoutes.Tags));
+              }}
+            >
+              TAGS
+            </MenuItem>
+          </MenuList>
+        </Menu>
+      </Flex>
+    </Flex>
+  );
+};
 
-export default MobileLayoutHeader
+export default MobileLayoutHeader;
