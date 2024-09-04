90cf7824b491eb8fb02d273eb6ce9ace8334c5f2
chobolevel <rodaka123@naver.com>
2024-09-04 23:43:56 +0900

    Refactor: add spinner when loading

--- src/components/domains/post/PostListItem.tsx
+++ src/components/domains/post/PostListItem.tsx
@@ -1,6 +1,6 @@
 import { Post } from "@/apis";
-import { Flex, Text } from "@chakra-ui/react";
-import { useMemo } from "react";
+import { Flex, Spinner, Text } from "@chakra-ui/react";
+import { useMemo, useState } from "react";
 import { DateUtils, toUrl } from "@/utils";
 import { useSafePush } from "@/hooks";
 import { PageRoutes } from "@/constants";
@@ -11,6 +11,7 @@ interface PostListItemProps {
 
 const PostListItem = ({ post }: PostListItemProps) => {
   const { push } = useSafePush();
+  const [loading, setLoading] = useState<boolean>(false);
 
   const writtenAt = useMemo(
     () => DateUtils.format(new Date(post.created_at), "YYYY-MM-DD"),
@@ -25,18 +26,29 @@ const PostListItem = ({ post }: PostListItemProps) => {
       borderColor={"lightGreen"}
       cursor={"pointer"}
       onClick={() => {
-        push(toUrl(PageRoutes.PostDetailById, { id: post.id }));
+        setLoading(true);
+        push(toUrl(PageRoutes.PostDetailById, { id: post.id }))?.then(() => {
+          setLoading(false);
+        });
       }}
     >
-      <Flex direction={"column"} gap={2}>
-        <Text fontSize={"lg"} fontWeight={"bold"}>
-          {post.title}
-        </Text>
-        <Text>{post.sub_title}</Text>
-      </Flex>
-      <Flex justify={"end"} align={"center"}>
-        <Text>{`작성일 ${writtenAt}`}</Text>
-      </Flex>
+      {loading ? (
+        <Flex direction={"column"} h={100} justify={"center"} align={"center"}>
+          <Spinner />
+        </Flex>
+      ) : (
+        <>
+          <Flex direction={"column"} gap={2}>
+            <Text fontSize={"lg"} fontWeight={"bold"}>
+              {post.title}
+            </Text>
+            <Text>{post.sub_title}</Text>
+          </Flex>
+          <Flex justify={"end"} align={"center"}>
+            <Text>{`작성일 ${writtenAt}`}</Text>
+          </Flex>
+        </>
+      )}
     </Flex>
   );
 };
