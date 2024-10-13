bcc2cb92c82e787d26f9e4e3b85469c89ac57e93
chobolevel <rodaka123@naver.com>
2024-10-13 18:19:26 +0900

    Style: apply customer style at editor content

--- src/components/providers/ChakraProvider/theme.ts
+++ src/components/providers/ChakraProvider/theme.ts
@@ -17,6 +17,7 @@ const theme = extendTheme({
   },
   styles: {
     global: {
+      // heading
       "div.sanitize-text-area h1": {
         fontSize: "3.5rem",
         fontWeight: "bold",
@@ -53,29 +54,79 @@ const theme = extendTheme({
         marginTop: "30px",
         marginBottom: "10px",
       },
-      ul: {
-        marginLeft: "30px",
+      "div.sanitize-text-area del": {
+        color: "#777980",
+      },
+      // quote
+      "div.sanitize-text-area blockquote": {
+        borderLeft: "6px solid gray",
+        paddingLeft: "10px",
+        marginY: "10px",
+        fontWeight: "bold",
+      },
+      // list(ol, ul)
+      "div.sanitize-text-area ul": {
+        marginLeft: "20px",
       },
       "div.sanitize-text-area ol": {
-        marginLeft: "30px",
+        marginLeft: "20px",
       },
       "div.sanitize-text-area li": {
         margin: "10px 0",
       },
+      // check list
+      "div.sanitize-text-area ul li.task-list-item": {
+        display: "flex",
+        alignItems: "center",
+        gap: "10px",
+        listStyle: "none",
+      },
+      "div.sanitize-text-area ul li.task-list-item::before": {
+        content: `"✅"`,
+      },
+      "div.sanitize-text-area table": {
+        borderCollapse: "collapse",
+      },
+      "div.sanitize-text-area thead": {
+        backgroundColor: "#38a16a",
+      },
+      "div.sanitize-text-area table, div.sanitize-text-area th, div.sanitize-text-area td":
+        {
+          padding: "10px",
+          border: "1px solid #38a16a",
+        },
+      // img
       "div.sanitize-text-area img": {
         marginTop: "20px",
         maxWidth: "800px",
         borderRadius: "10px",
       },
+      // a
       "div.sanitize-text-area a:hover": {
         fontWeight: "bold",
+        color: "#38a16a",
       },
-      ".toastui-editor-ww-code-block": {
+      // code block
+      "div.sanitize-text-area code": {
+        padding: "6px",
+        color: "#EB5757",
+        backgroundColor: "rgba(135, 131, 120, .15)",
+        borderRadius: "10px",
+      },
+      "div.sanitize-text-area .toastui-editor-ww-code-block": {
+        marginY: "20px",
         overflowX: "auto",
         borderRadius: "10px",
         padding: "20px",
-        backgroundColor: "#eee",
+        backgroundColor: "rgba(135, 131, 120, .15)",
         textWrap: "wrap",
+      },
+      "div.sanitize-text-area .toastui-editor-ww-code-block code": {
+        padding: "unset",
+        border: "none",
+        color: "var(--chakra-colors-chakra-body-text)",
+        backgroundColor: "transparent",
+        borderRadius: "unset",
       },
     },
   },
