1e2e14baf52ece179140ae0a47fb71c1e4b0754d
chobolevel <rodaka123@naver.com>
2024-12-13 13:36:38 +0900

    Fix: editor theme issue

--- src/components/common/WysiwygEditor/WysiwygViewer.tsx
+++ src/components/common/WysiwygEditor/WysiwygViewer.tsx
@@ -5,13 +5,21 @@ import "@toast-ui/editor/dist/theme/toastui-editor-dark.css";
 import "prismjs/themes/prism.css";
 import "@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight.css"; // @ts-ignore
 import codeSyntaxHighlight from "@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight-all";
+import { useColorModeValue } from "@chakra-ui/react";
 
 interface WysiwygViewerProps {
   value: string;
 }
 
 const WysiwygViewer = ({ value }: WysiwygViewerProps) => {
-  return <Viewer plugins={[codeSyntaxHighlight]} initialValue={value} />;
+  const editorTheme = useColorModeValue("white", "dark");
+  return (
+    <Viewer
+      theme={editorTheme}
+      plugins={[codeSyntaxHighlight]}
+      initialValue={value}
+    />
+  );
 };
 
 export default WysiwygViewer;
