88842fe52245162e2709378968edaadbfc4f40e4
chobolevel <rodaka123@naver.com>
2026-09-11 15:22:33 +0900

    feat: 토스트 닫기 버튼 추가

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -39,7 +39,7 @@ export default function Providers({ children }: { children: React.ReactNode }) {
         disableTransitionOnChange
       >
         {children}
-        <Toaster richColors position="top-right" />
+        <Toaster richColors closeButton position="top-right" />
       </ThemeProvider>
       {DevTools && <DevTools initialIsOpen={false} />}
     </QueryClientProvider>
